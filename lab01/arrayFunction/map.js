8e54f5e1c17509015dc123fbc386005045be34ae
TuongVan <nguyenngoctuongvan3003@gmail.com>
2024-08-29 08:59:26 +0700

    tuan2 lab1

--- lab01/arrayFunction/map.js
+++ lab01/arrayFunction/map.js
@@ -8,10 +8,19 @@ const num=[1,4,9]
 const roots=num.map((num)=>num**2)//Math.sqrt(num)
 console.log(roots);
 
+//callback
 var number=[30,3,2003]
-
 Array.prototype.mapfn=function(callback){
-    callback();
-    
+    let newarr=[];
+    var length=this.length;
+    for (let index = 0; index < length; index++) {
+       newarr.push(callback(this[index]))
+    }
+    return newarr;
 }
+var num2= number.mapfn((ele)=>{
+    return ele+3
+})
+console.log(num2);
+
   
